9a5464359a792a1e0e1cb1beece6a9932a82289a
Akin Bektas <akin_bektas@hotmail.com>
2015-09-17 11:47:21 +0200

    Practicum 3

---
+++ Practicum 3/Data/Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Data
+//
+//  Created by Akin Bektas on 17-09-15.
+//  Copyright (c) 2015 Akin Bektas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
